3f9086010e7cf841065576d0cef46fc1309cb405
AmyHuang82 <a3315520@gmail.com>
2021-05-08 15:08:54 +0800

    Add: hover effect to Card

--- components/Illustrations/Card/Card.style.js
+++ components/Illustrations/Card/Card.style.js
@@ -1,7 +1,17 @@
 import styled from 'styled-components'
+import { transparentize } from 'polished'
+import { NOBEL_GRAY } from 'constants/colors'
 
 export const Wrapper = styled.div`
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
+
+  :hover {
+    > div {
+      box-shadow: 0 16px 22px 0 ${transparentize(0.3, NOBEL_GRAY)};
+      top: -5px;
+    }
+  }
 `
