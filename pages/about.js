1d50cffd2599e071062533b5de9657cac2ec0771
AmyHuang82 <a3315520@gmail.com>
2021-05-11 16:24:33 +0800

    Add: Footer to about & project page

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Header from 'components/Header/Header'
+import Footer from 'components/Footer/Footer'
 import { Container } from 'components/Common/style'
 import { BASE_TITLE, ABOUT_PAGE } from 'constants/headInfo'
 
@@ -12,6 +13,7 @@ function About() {
       <Header isAbout />
       <Container>
         <div>About</div>
+        <Footer />
       </Container>
     </>
   )
