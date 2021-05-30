7069959b3947dca12a8fe19ddd823b6f13adad84
AmyHuang82 <a3315520@gmail.com>
2021-05-30 21:55:05 +0800

    Add: /api/illustration

---
+++ pages/api/illustration.js
@@ -0,0 +1,19 @@
+import db from 'firebase'
+
+async function handler(req, res) {
+  try {
+    if (req.method === 'GET') {
+      const snapshot = await db.collection('illustration').get()
+      if (snapshot.empty) {
+        res.status(404).end()
+      } else {
+        const data = snapshot.docs.map(doc => doc.data())
+        res.status(200).json(data)
+      }
+    }
+  } catch (error) {
+    res.status(400).json(error)
+  }
+}
+
+export default handler
