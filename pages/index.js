da0ddb160d6bc12ae604fe8d612ac1388d5cd238
AmyHuang82 <a3315520@gmail.com>
2021-05-31 23:06:59 +0800

    Remove: unused getStaticProps & getIllustrationsInfo

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,10 @@ import Head from 'next/head'
 import Header from 'components/Header/Header'
 import Footer from 'components/Footer/Footer'
 import Illustrations from 'components/Illustrations/Illustrations'
-import getIllustrationsInfo from 'lib/getIllustrationsInfo'
 import { Container } from 'components/Common/style'
 import { BASE_TITLE, ILLUSTRATION_PAGE } from 'constants/headInfo'
 
-export function getStaticProps() {
-  const illustrations = getIllustrationsInfo()
-  return { props: { illustrations } }
-}
-
-function Home({ illustrations }) {
+function Home() {
   return (
     <>
       <Head>
@@ -19,7 +13,7 @@ function Home({ illustrations }) {
       </Head>
       <Header isIllustration />
       <Container>
-        <Illustrations list={illustrations} />
+        <Illustrations />
         <Footer />
       </Container>
     </>
