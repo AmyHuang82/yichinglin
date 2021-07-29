c699d58b88a61063bf7d86f21af631f8cf915ab8
AmyHuang82 <a3315520@gmail.com>
2021-07-29 22:50:48 +0800

    Add: YichingLin to about page

---
+++ components/YichingLin/YichingLin.js
@@ -0,0 +1,65 @@
+import { INSTAGRAM_LINK, PINKOI_LINK, MAIL, MAIL_LINK } from 'constants/links'
+import IG from '../icons/Instagram'
+import Pinkoi from '../icons/Pinkoi'
+import {
+  Avatar,
+  Wrapper,
+  InfoWrapper,
+  Intro,
+  Contact,
+} from './YichingLin.style'
+
+function YichingLin() {
+  return (
+    <Wrapper>
+      <Avatar />
+      <InfoWrapper>
+        <Intro>
+          從小喜歡繪本、喜歡畫畫，
+          <br />
+          長大後跟著爸爸走入山林、愛上賞鳥。
+          <br />
+          希望能將大自然給予的種種驚喜，
+          <br />
+          透過一支畫筆分享給大家。
+        </Intro>
+
+        <div>
+          大學念傳播行銷，畢業後做了幾年行銷工作，卻從來沒有忘記對畫畫的熱情，28歲正式開始以創作為生。
+          <br />
+          喜歡畫各種溫暖美好的人事物，尤其享受描繪野鳥、野生動物、生態相關的題材。希望世界上每個生命都有屬於自己的一方天地，和諧存續。
+        </div>
+
+        <Contact>
+          <Contact.Title>Contact</Contact.Title>
+          <div>
+            合作邀約歡迎email聯繫。
+            <br />
+            Please email me at &nbsp;
+            <Contact.Email href={MAIL_LINK}>{MAIL}</Contact.Email>
+          </div>
+          <Contact.Links>
+            <Contact.IconLink
+              target="_blank"
+              rel="noreferrer"
+              href={INSTAGRAM_LINK}
+            >
+              <IG />
+              its_yiching
+            </Contact.IconLink>
+            <Contact.IconLink
+              target="_blank"
+              rel="noreferrer"
+              href={PINKOI_LINK}
+            >
+              <Pinkoi />
+              Ching Illustration
+            </Contact.IconLink>
+          </Contact.Links>
+        </Contact>
+      </InfoWrapper>
+    </Wrapper>
+  )
+}
+
+export default YichingLin
---
+++ components/YichingLin/YichingLin.style.js
@@ -0,0 +1,87 @@
+import styled from 'styled-components'
+import { media } from 'utils/styleUtils'
+import { BLACK, NOBEL_GRAY, CAMOUFLAGE_GREEN } from 'constants/colors'
+
+const AVATAR_MARGIN_RIGHT = 50
+const AVATAR_DESKTOP_WIDTH = 45
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding-bottom: 100px;
+`
+
+export const Avatar = styled.div`
+  flex-grow: 0;
+  width: 100%;
+  padding-bottom: 100%;
+  background-color: ${NOBEL_GRAY};
+
+  ${media.pad} {
+    width: ${AVATAR_DESKTOP_WIDTH}%;
+    padding-bottom: ${AVATAR_DESKTOP_WIDTH}%;
+    margin-right: ${AVATAR_MARGIN_RIGHT}px;
+  }
+`
+
+export const InfoWrapper = styled.div`
+  width: 100%;
+  line-height: 1.75rem;
+  font-size: 0.875rem;
+
+  ${media.pad} {
+    width: calc(${100 - AVATAR_DESKTOP_WIDTH}% - ${AVATAR_MARGIN_RIGHT}px);
+    font-size: 1rem;
+  }
+`
+
+export const Intro = styled.div`
+  padding: 35px 0;
+  text-align: center;
+  color: ${CAMOUFLAGE_GREEN};
+`
+
+export const Contact = styled.div`
+  padding: 35px 0;
+`
+
+Contact.Title = styled.div`
+  font-size: 1.25rem;
+  font-weight: 700;
+  color: ${CAMOUFLAGE_GREEN};
+  margin-bottom: 8px;
+`
+
+Contact.Email = styled.a`
+  color: ${CAMOUFLAGE_GREEN};
+`
+
+Contact.IconLink = styled.a`
+  display: flex;
+  align-items: center;
+  width: fit-content;
+
+  :visited {
+    color: ${BLACK};
+  }
+
+  svg {
+    width: 20px;
+    margin-right: 10px;
+  }
+
+  :hover {
+    color: ${NOBEL_GRAY};
+    path {
+      fill: ${NOBEL_GRAY};
+    }
+  }
+`
+
+Contact.Links = styled.div`
+  display: flex;
+
+  ${Contact.IconLink} + ${Contact.IconLink} {
+    margin-left: 1.5rem;
+  }
+`
