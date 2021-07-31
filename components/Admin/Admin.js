98924b605987147992bc3e1f0544df6feaf6918d
AmyHuang82 <a3315520@gmail.com>
2021-07-31 18:27:58 +0800

    Update: Admin with Menu

--- components/Admin/Admin.js
+++ components/Admin/Admin.js
@@ -1,12 +1,35 @@
 import { useState } from 'react'
+import { Menu } from 'antd'
 import LoginModal from './LoginModal'
+
+const ILLUSTRATION_KEY = 'illustration'
+const PROJECT_KEY = 'project'
 
 function Admin() {
   const [loginUid, setLoginUid] = useState(null)
   const isLogin = loginUid === process.env.NEXT_PUBLIC_VALID_UID
 
+  const [currentKey, setCurrentKey] = useState(ILLUSTRATION_KEY)
+
+  function onMenuItemClick(e) {
+    setCurrentKey(e.key)
+  }
+
   if (!isLogin) return <LoginModal setLoginUid={setLoginUid} />
-  return <div>管理者後台</div>
+  return (
+    <>
+      <Menu
+        mode="horizontal"
+        selectedKeys={[currentKey]}
+        onClick={onMenuItemClick}
+      >
+        <Menu.Item key={ILLUSTRATION_KEY}>Illustration 管理</Menu.Item>
+        <Menu.Item key={PROJECT_KEY}>Project 管理</Menu.Item>
+      </Menu>
+      {currentKey === ILLUSTRATION_KEY && <div>Illustration 管理</div>}
+      {currentKey === PROJECT_KEY && <div>Project 管理</div>}
+    </>
+  )
 }
 
 export default Admin
