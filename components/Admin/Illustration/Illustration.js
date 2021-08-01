02bdf967ee2baf11abe616080f6a8b111b80a25a
AmyHuang82 <a3315520@gmail.com>
2021-08-01 09:23:14 +0800

    Refactor: move List into Illustration

--- components/Admin/Illustration/Illustration.js
+++ components/Admin/Illustration/Illustration.js
@@ -1,10 +1,44 @@
-import { Container } from '../Admin.style'
-import List from './List'
+import { Button, Table } from 'antd'
+import useIllustration from 'components/api/useIllustration'
+import { Container, Row } from '../Admin.style'
 
 function Illustration() {
+  const { data } = useIllustration()
+
   return (
     <Container>
-      <List />
+      <Row>
+        <h2>Illustration 列表</h2>
+        <Button type="primary">+ 新增作品</Button>
+      </Row>
+      <Table
+        rowKey="id"
+        dataSource={data}
+        pagination={false}
+        scroll={{ y: '60vh' }}
+        columns={[
+          {
+            title: '排序',
+            key: 'index',
+            render: (_, __, index) => index + 1,
+          },
+          { title: '名稱', dataIndex: 'name' },
+          {
+            title: '尺寸（寬 x 高）',
+            key: 'size',
+            render: ({ width, height }) => `${width} x ${height}`,
+          },
+          {
+            title: '圖片連結',
+            dataIndex: 'src',
+            render: src => (
+              <a target="_blank" rel="noreferrer" href={src}>
+                點擊查看
+              </a>
+            ),
+          },
+        ]}
+      />
     </Container>
   )
 }
