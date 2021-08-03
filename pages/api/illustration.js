f14e92c18c3ec1dcae0ceff94f084ed0db66baf9
AmyHuang82 <a3315520@gmail.com>
2021-08-03 21:58:37 +0800

    Fix: async/await doesn't work with forEach https://stackoverflow.com/a/54095421

--- pages/api/illustration.js
+++ pages/api/illustration.js
@@ -22,7 +22,6 @@ async function handler(req, res) {
         res.status(200).json([])
       } else {
         const data = snapshot.docs.map(doc => ({
-          id: doc.id,
           ...doc.data(),
         }))
         res.status(200).json(data)
@@ -30,39 +29,51 @@ async function handler(req, res) {
     }
     if (req.method === 'POST') {
       const form = formidable({ multiples: true })
-      form.parse(req, (error, fields, files) => {
+      form.parse(req, async (error, fields, files) => {
         if (error) res.status(400).json(error)
 
-        const parsedFields = parseFormObject(fields)
         const parsedFiles = parseFormObject(files)
+        const parsedFields = parseFormObject(fields)
+        const uploadInfos = []
 
-        Object.keys(parsedFields).forEach(async key => {
-          const file = parsedFiles[key].imageFile
+        Object.keys(parsedFields).forEach(key => {
           const token = uuid()
+          const file = parsedFiles[key].imageFile
+          const filePath = file.path
           const remotePath = `illustration/${file.name}`
+          uploadInfos.push({
+            filePath,
+            remotePath,
+            token,
+          })
+          parsedFields[key].src = createFirebaseStorageUrl(remotePath, token)
 
-          await bucket
-            .upload(file.path, {
-              destination: remotePath,
+          const docId = uuid()
+          parsedFields[key].id = docId
+        })
+
+        await Promise.all(
+          uploadInfos.map(info => {
+            return bucket.upload(info.filePath, {
+              destination: info.remotePath,
               metadata: {
                 cacheControl: 'public, max-age=604800',
                 metadata: {
-                  firebaseStorageDownloadTokens: token,
+                  firebaseStorageDownloadTokens: info.token,
                 },
               },
             })
-            .catch(error => {
-              res.status(400).json(error)
-            })
+          })
+        ).catch(error => {
+          res.status(400).json(error)
+        })
 
-          parsedFields[key].src = createFirebaseStorageUrl(remotePath, token)
-
-          await db
-            .collection('illustration')
-            .add(parsedFields[key])
-            .catch(error => {
-              res.status(400).json(error)
-            })
+        await Promise.all(
+          Object.values(parsedFields).map(field => {
+            return db.collection('illustration').doc(field.id).set(field)
+          })
+        ).catch(error => {
+          res.status(400).json(error)
         })
 
         res.status(201).json(Object.values(parsedFields))
