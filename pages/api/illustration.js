3f282e4baa962be719bef60afafa02799debdeec
AmyHuang82 <a3315520@gmail.com>
2021-08-03 21:13:22 +0800

    Fix: api no data should return empty array

--- pages/api/illustration.js
+++ pages/api/illustration.js
@@ -19,7 +19,7 @@ async function handler(req, res) {
     if (req.method === 'GET') {
       const snapshot = await db.collection('illustration').get()
       if (snapshot.empty) {
-        res.status(404).end()
+        res.status(200).json([])
       } else {
         const data = snapshot.docs.map(doc => ({
           id: doc.id,
