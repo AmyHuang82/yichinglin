eadba96e77650b2c2941a1267d87cfa01294ca9a
AmyHuang82 <a3315520@gmail.com>
2021-08-07 17:24:05 +0800

    Add: useDeleteIllustration

---
+++ components/api/useDeleteIllustration.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+import { useMutation } from 'react-query'
+
+function deleteIllustrationAPI({ id, name }) {
+  return axios
+    .delete(`/api/illustration/${id}`, { params: { imageName: name } })
+    .catch(error => {
+      if (error.response) throw new Error(error.response.data.message)
+      throw error
+    })
+}
+
+function useDeleteIllustration() {
+  const { mutate: submit, isLoading } = useMutation(deleteIllustrationAPI)
+  return { submit, isLoading }
+}
+export default useDeleteIllustration
