03a848aed82e7c064cd54e63c8c1e10db7ead210
AmyHuang82 <a3315520@gmail.com>
2021-08-08 17:42:01 +0800

    Update: pages title & description

--- constants/headInfo.js
+++ constants/headInfo.js
@@ -1,4 +1,12 @@
-export const BASE_TITLE = 'Yiching Lin'
+export const BASE_TITLE = 'Yiching Lin Illustration'
 export const ILLUSTRATION_PAGE = 'Illustration'
 export const PROJECT_PAGE = 'Project'
 export const ABOUT_PAGE = 'About'
+
+export const DESCRIPTIONS = {
+  ILLUSTRATION_PAGE:
+    '一個喜愛野鳥的自由插畫家——以晴的作品集。Illustration portfolio of a Taiwanese illustrator who loves wild bird.',
+  PROJECT_PAGE: '插畫合作案例。',
+  ABOUT_PAGE:
+    '從小喜歡繪本、喜歡畫畫，長大後跟著爸爸走入山林、愛上賞鳥。希望能將大自然給予的種種驚喜，透過一支畫筆分享給大家。',
+}
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,18 @@ import Header from 'components/Header/Header'
 import Footer from 'components/Footer/Footer'
 import Illustrations from 'components/Illustrations/Illustrations'
 import { Container } from 'components/Common/style'
-import { BASE_TITLE, ILLUSTRATION_PAGE } from 'constants/headInfo'
+import { BASE_TITLE, DESCRIPTIONS } from 'constants/headInfo'
 
 function HomePage() {
   return (
     <>
       <Head>
-        <title>{`${BASE_TITLE} - ${ILLUSTRATION_PAGE}`}</title>
+        <title>{BASE_TITLE}</title>
+        <meta name="description" content={DESCRIPTIONS.ILLUSTRATION_PAGE} />
+        <meta property="og:url" content="https://yichinglin.vercel.app" />
+        <meta property="og:site_name" content={BASE_TITLE} />
+        <meta property="og:title" content={BASE_TITLE} />
+        <meta name="og:description" content={DESCRIPTIONS.ILLUSTRATION_PAGE} />
       </Head>
       <Header isIllustration />
       <Container>
