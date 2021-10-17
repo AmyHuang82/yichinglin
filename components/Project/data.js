031eeb9b69f77132de20d52789daaf2cc170911b
AmyHuang82 <a3315520@gmail.com>
2021-10-17 17:57:43 +0800

    Add: forest project

--- components/Project/data.js
+++ components/Project/data.js
@@ -11,8 +11,30 @@ const data = [
       label: '〈森林之歌〉，希塔動物溝通Facebook粉絲專頁Cover Photo。',
     },
     client: '希塔動物溝通',
-    html:
-      '<p>動物溝通師是飼主與寵物的橋樑，能夠接收動物的意念並轉譯為人們理解的語言。與動物連線彷彿進入一個不同維度的空間，完全敞開並感受視覺、聲音、氣味等各種形式的訊息。溝通師向我分享這個過程「是一種很正向、開心、聊天的感覺，動物的能量都很棒」。</p><figure><img src="https://firebasestorage.googleapis.com/v0/b/ching-illustration.appspot.com/o/project%2F%E5%8B%95%E7%89%A9%E6%BA%9D%E9%80%9A_%E5%85%A7%E6%96%87_%E9%81%8E%E7%A8%8B.jpg?alt=media&token=0d947776-7121-4a49-9101-a480e30d2547" /><figcaption>〈森林之歌〉，希塔動物溝通官方LINE選單草稿及色彩概念。</figcaption></figure><p>〈森林之歌〉描繪的是一座奇幻又療癒的森林，動物們在其中自由敲響飄動的弦，豐富的訊息隨風飛揚。並以粉紫、粉橘為主色調，傳遞更加溫柔神秘的氛圍。</p><figure><img src="https://firebasestorage.googleapis.com/v0/b/ching-illustration.appspot.com/o/project%2F%E5%8B%95%E7%89%A9%E6%BA%9D%E9%80%9A_%E5%85%A7%E6%96%87_Line.jpg?alt=media&token=7cc61047-3899-46db-8d21-77d31447ed84" /><figcaption>〈森林之歌〉，希塔動物溝通官方LINE選單。</figcaption></figure>',
+    html: `<p>動物溝通師是飼主與寵物的橋樑，能夠接收動物的意念並轉譯為人們理解的語言。與動物連線彷彿進入一個不同維度的空間，完全敞開並感受視覺、聲音、氣味等各種形式的訊息。溝通師向我分享這個過程「是一種很正向、開心、聊天的感覺，動物的能量都很棒」。</p>
+      <figure><img src="https://firebasestorage.googleapis.com/v0/b/ching-illustration.appspot.com/o/project%2F%E5%8B%95%E7%89%A9%E6%BA%9D%E9%80%9A_%E5%85%A7%E6%96%87_%E9%81%8E%E7%A8%8B.jpg?alt=media&token=0d947776-7121-4a49-9101-a480e30d2547" /><figcaption>〈森林之歌〉，希塔動物溝通官方LINE選單草稿及色彩概念。</figcaption></figure>
+      <p>〈森林之歌〉描繪的是一座奇幻又療癒的森林，動物們在其中自由敲響飄動的弦，豐富的訊息隨風飛揚。並以粉紫、粉橘為主色調，傳遞更加溫柔神秘的氛圍。</p>
+      <figure><img src="https://firebasestorage.googleapis.com/v0/b/ching-illustration.appspot.com/o/project%2F%E5%8B%95%E7%89%A9%E6%BA%9D%E9%80%9A_%E5%85%A7%E6%96%87_Line.jpg?alt=media&token=7cc61047-3899-46db-8d21-77d31447ed84" /><figcaption>〈森林之歌〉，希塔動物溝通官方LINE選單。</figcaption></figure>`,
+  },
+  {
+    id: 2,
+    title: '主視覺插畫｜屏東林業小棧 職人森活指南',
+    description: '林務局屏東林區管理處活動海報、製作物',
+    cover:
+      'https://firebasestorage.googleapis.com/v0/b/ching-illustration.appspot.com/o/project%2F%E6%9E%97%E6%A5%AD%E5%B0%8F%E6%A3%A7_%E9%A6%96%E5%9C%96.jpg?alt=media&token=294f2a9b-0a9e-4c28-b2ef-887b405ee91e',
+    contentCover: {
+      url:
+        'https://firebasestorage.googleapis.com/v0/b/ching-illustration.appspot.com/o/project%2F%E6%9E%97%E6%A5%AD%E5%B0%8F%E6%A3%A7_%E5%85%A7%E6%96%87_%E4%B8%BB%E8%A6%96%E8%A6%BA.jpg?alt=media&token=888c13e6-002c-4bb3-907e-f9cb15c9f899',
+      label: '〈屏東林業小棧 職人森活指南〉主視覺插畫。',
+    },
+    client: '林務局屏東林區管理處、斯創教育工作群',
+    html: `<p>「屏東林業小棧 職人森活指南」活動由林務局屏東林區管理處主辦、斯創教育工作群執行，連續3個月邀請不同領域職人來到屏東林業小棧分享創作，同時還有豐富的手作活動，邀請民眾在職人的帶領下一起體驗創作的療癒。</p>
+      <figure><img src="https://firebasestorage.googleapis.com/v0/b/ching-illustration.appspot.com/o/project%2F%E6%9E%97%E6%A5%AD%E5%B0%8F%E6%A3%A7_%E5%85%A7%E6%96%87_%E4%B8%BB%E8%A6%96%E8%A6%BA%E6%B5%B7%E5%A0%B1.jpg?alt=media&token=1b422f85-e7d2-4b37-b2c3-e234e4d30c08" /><figcaption>〈屏東林業小棧 職人森活指南〉主視覺海報。</figcaption></figure>
+      <figure><img src="https://firebasestorage.googleapis.com/v0/b/ching-illustration.appspot.com/o/project%2F%E6%9E%97%E6%A5%AD%E5%B0%8F%E6%A3%A7_%E5%85%A7%E6%96%87_%E5%96%AE%E5%A0%B4%E6%AC%A1%E6%B5%B7%E5%A0%B1.jpg?alt=media&token=9d1ec851-07b7-43c4-a227-198647985f88" /><figcaption>〈屏東林業小棧 職人森活指南〉單場次宣傳海報。</figcaption></figure>
+      <p>本次主視覺插畫在簡潔清新的氛圍下，描繪職人與民眾在屏東林業小棧共同度過美好週末的景象，徐徐微風下，明亮的黃、綠、藍交織出自然的舒心，手繪線條帶出人們相聚的溫度。</p>
+      <figure><img src="https://firebasestorage.googleapis.com/v0/b/ching-illustration.appspot.com/o/project%2F%E6%9E%97%E6%A5%AD%E5%B0%8F%E6%A3%A7_%E5%85%A7%E6%96%87_%E8%81%B7%E4%BA%BA.jpg?alt=media&token=f04f185d-430c-417b-9179-f81055e3584f" /><figcaption>〈屏東林業小棧 職人森活指南〉各場次職人。</figcaption></figure>
+      <figure><img src="https://firebasestorage.googleapis.com/v0/b/ching-illustration.appspot.com/o/project%2F%E6%9E%97%E6%A5%AD%E5%B0%8F%E6%A3%A7_%E5%85%A7%E6%96%87_%E8%A1%8D%E7%94%9F%E7%89%A9.jpg?alt=media&token=6c3d418d-0cd2-4395-bdcf-9132ee2cf7d2" /><figcaption>〈屏東林業小棧 職人森活指南〉帆布(左)及關東旗(右)。照片感謝斯創教育工作群提供。</figcaption></figure>
+      `,
   },
 ]
 
