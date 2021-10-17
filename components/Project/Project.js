37389214d01cc325a2ce7f518fa3a900101ad60a
AmyHuang82 <a3315520@gmail.com>
2021-10-17 13:01:43 +0800

    Add: animal whisperer project card data

--- components/Project/Project.js
+++ components/Project/Project.js
@@ -3,15 +3,11 @@ import Card from './Card/Card'
 
 const data = [
   {
-    id: 1,
+    id: 'animal_whisperer',
     title: '品牌形象插畫｜動物溝通',
-    subtitle: '這個是與別人合作，關於動物溝通的插畫',
-    cover: 'https://yichinglin.vercel.app/main_image.jpg',
-  },
-  {
-    id: 1,
-    title: '品牌形象插畫｜動物溝通',
-    cover: 'https://yichinglin.vercel.app/main_image.jpg',
+    subtitle: '希塔動物溝通 Facebook Cover Photo & Line 選單',
+    cover:
+      'https://firebasestorage.googleapis.com/v0/b/ching-illustration.appspot.com/o/project%2Fanimal_whisperer%2F%E5%8B%95%E7%89%A9%E6%BA%9D%E9%80%9A_%E9%A6%96%E5%9C%96.jpg?alt=media&token=6bd602db-29af-457b-a6ea-e8d9d9f46d33',
   },
 ]
 
