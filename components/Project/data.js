a8311ea7e8e11873ab5b5a36dd0a5ab20e45bb4f
AmyHuang82 <a3315520@gmail.com>
2021-11-17 15:19:34 +0800

    Update: project data

--- components/Project/data.js
+++ components/Project/data.js
@@ -3,7 +3,7 @@ const data = [
     id: 1,
     order: 2,
     title: '品牌形象插畫｜希塔動物溝通',
-    description: '希塔動物溝通 Facebook Cover Photo & Line 選單',
+    description: '粉絲專頁 Cover Photo & Line 官方帳號選單',
     cover:
       'https://firebasestorage.googleapis.com/v0/b/ching-illustration.appspot.com/o/project%2F%E5%8B%95%E7%89%A9%E6%BA%9D%E9%80%9A_%E9%A6%96%E5%9C%96.jpg?alt=media&token=964c9244-3904-4f94-a235-88c321274eb7',
     contentCover: {
@@ -20,8 +20,8 @@ const data = [
   {
     id: 2,
     order: 3,
-    title: '主視覺插畫｜屏東林業小棧 職人森活指南',
-    description: '林務局屏東林區管理處活動海報、製作物',
+    title: '主視覺｜屏東林業小棧 職人森活指南',
+    description: '活動宣傳海報及製作物',
     cover:
       'https://firebasestorage.googleapis.com/v0/b/ching-illustration.appspot.com/o/project%2F%E6%9E%97%E6%A5%AD%E5%B0%8F%E6%A3%A7_%E9%A6%96%E5%9C%96.jpg?alt=media&token=294f2a9b-0a9e-4c28-b2ef-887b405ee91e',
     contentCover: {
@@ -31,7 +31,7 @@ const data = [
     },
     client: '林務局屏東林區管理處、斯創教育工作群',
     html: `<p>「屏東林業小棧 職人森活指南」活動由林務局屏東林區管理處主辦、斯創教育工作群執行，連續3個月邀請不同領域職人來到屏東林業小棧分享創作，同時還有豐富的手作活動，邀請民眾在職人的帶領下一起體驗創作的療癒。</p>
-      <figure><img src="https://firebasestorage.googleapis.com/v0/b/ching-illustration.appspot.com/o/project%2F%E6%9E%97%E6%A5%AD%E5%B0%8F%E6%A3%A7_%E5%85%A7%E6%96%87_%E4%B8%BB%E8%A6%96%E8%A6%BA%E6%B5%B7%E5%A0%B1.jpg?alt=media&token=1b422f85-e7d2-4b37-b2c3-e234e4d30c08" /><figcaption>〈屏東林業小棧 職人森活指南〉主視覺海報。</figcaption></figure>
+      <figure><img src="https://firebasestorage.googleapis.com/v0/b/ching-illustration.appspot.com/o/project%2F%E6%9E%97%E6%A5%AD%E5%B0%8F%E6%A3%A7_%E5%85%A7%E6%96%87_%E4%B8%BB%E8%A6%96%E8%A6%BA%E6%B5%B7%E5%A0%B1.jpg?alt=media&token=71f4e414-44f7-476a-9451-2217de5ad808" /><figcaption>〈屏東林業小棧 職人森活指南〉主視覺海報。</figcaption></figure>
       <figure><img src="https://firebasestorage.googleapis.com/v0/b/ching-illustration.appspot.com/o/project%2F%E6%9E%97%E6%A5%AD%E5%B0%8F%E6%A3%A7_%E5%85%A7%E6%96%87_%E5%96%AE%E5%A0%B4%E6%AC%A1%E6%B5%B7%E5%A0%B1.jpg?alt=media&token=9d1ec851-07b7-43c4-a227-198647985f88" /><figcaption>〈屏東林業小棧 職人森活指南〉單場次宣傳海報。</figcaption></figure>
       <p>本次主視覺插畫在簡潔清新的氛圍下，描繪職人與民眾在屏東林業小棧共同度過美好週末的景象，徐徐微風下，明亮的黃、綠、藍交織出自然的舒心，手繪線條帶出人們相聚的溫度。</p>
       <figure><img src="https://firebasestorage.googleapis.com/v0/b/ching-illustration.appspot.com/o/project%2F%E6%9E%97%E6%A5%AD%E5%B0%8F%E6%A3%A7_%E5%85%A7%E6%96%87_%E8%81%B7%E4%BA%BA.jpg?alt=media&token=f04f185d-430c-417b-9179-f81055e3584f" /><figcaption>〈屏東林業小棧 職人森活指南〉各場次職人。</figcaption></figure>
