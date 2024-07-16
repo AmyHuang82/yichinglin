31892d6a8dd91804b539312bedc7e2d3ff931899
AmyHuang82 <a3315520@gmail.com>
2024-07-17 00:33:03 +0800

    refactor: remove fbemitter

--- components/Illustrations/Card/Modal/Modal.js
+++ components/Illustrations/Card/Modal/Modal.js
@@ -1,6 +1,5 @@
 import { useEffect, useRef } from 'react'
 import { createPortal } from 'react-dom'
-import { EventEmitter } from 'fbemitter'
 import {
   noScroll,
   GlobalStyle,
@@ -36,26 +35,15 @@ function Portal({ children }) {
 function Modal({ data: { name, src, description }, closeModal }) {
   const maskRef = useRef()
 
-  useEffect(() => {
-    const emitter = new EventEmitter()
-
-    emitter.addListener('closeModal', e => {
-      if (e.target === maskRef.current) {
-        closeModal()
-      }
-    })
-    maskRef.current.addEventListener('click', e => {
-      emitter.emit('closeModal', e)
-    })
-
-    return () => {
-      emitter.removeAllListeners()
+  function onMaskClick(e) {
+    if (e.target === maskRef.current) {
+      closeModal()
     }
-  }, [])
+  }
 
   return (
     <Portal>
-      <Container ref={maskRef}>
+      <Container ref={maskRef} onClick={onMaskClick}>
         <Close onClick={closeModal} />
         <Content>
           <Image src={src} alt={name} />
