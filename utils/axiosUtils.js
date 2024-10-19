c3743a1e08d0959cdd61357f430770046e148a99
AmyHuang82 <a3315520@gmail.com>
2024-10-20 00:19:12 +0800

    fix: `baseURL`

--- utils/axiosUtils.js
+++ utils/axiosUtils.js
@@ -4,5 +4,5 @@ import axios from 'axios'
 const IS_DEV = process.env.NODE_ENV === 'development'
 
 export default axios.create({
-  baseURL: IS_DEV ? 'http://localhost:3000' : '',
+  baseURL: IS_DEV ? 'http://localhost:3000' : 'https://yichinglin.vercel.app',
 })
