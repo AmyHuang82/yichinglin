bcb93ac665e0cf493bcc247fd9386e6a5043ca3a
AmyHuang82 <a3315520@gmail.com>
2025-02-03 19:28:13 +0800

    enhance: update about content

--- components/About/About.js
+++ components/About/About.js
@@ -39,13 +39,20 @@ function About() {
         <Description>
           <div>
             你好，我是以晴。大學念傳播行銷，畢業後做了幾年行銷工作，28
-            歲時結合藝術與生態，正式開始以創作為生。喜歡細細觀察、研究與描繪野生動物，懷著分享知識與推廣保育的熱情。希望世界上每個生命都有屬於自己的一方天地，和諧存續。
+            歲時結合藝術與生態，正式開始以創作為生。喜歡細細觀察、研究與描繪野生動物，持續探索著科學精確性與藝術家個人詮釋之間的平衡。2024
+            年，我完成英國金士頓大學（Kingston University
+            London）的插畫碩士學業，這段經歷讓我以研究為基礎的創作方式得到更深也更廣的探索空間。懷著分享知識與推廣保育的熱情，我期許自己的作品能夠激發好奇心，讓人們開始去注意自己所處的環境。
           </div>
           <div>
-            Hello! I am Yiching, a Taiwanese illustrator and birder. Greatly
-            influenced by my lifelong passion for birdwatching, my illustration
-            practice centers around nature and wildlife, with a particular
-            interest in conservation education.
+            Hello! I am Yiching, a Taiwanese illustrator and birder. Inspired by
+            my lifelong passion for birdwatching, my work centers around nature
+            and wildlife, blending scientific accuracy with artistic
+            storytelling. I’m especially interested in conservation education,
+            using illustration to spark curiosity and connection with the
+            natural world. In 2024, I earned a Master&apos;s degree in
+            Illustration from Kingston University London, further shaping my
+            research-driven approach to visual narratives, combining fieldwork
+            and creative exploration to deepen my practice.
           </div>
         </Description>
 
@@ -61,18 +68,18 @@ function About() {
             <Contact.IconLink
               target="_blank"
               rel="noreferrer"
-              href={FACEBOOK_LINK}
-            >
-              <FB />
-              Yiching Lin Illustration
-            </Contact.IconLink>
-            <Contact.IconLink
-              target="_blank"
-              rel="noreferrer"
               href={INSTAGRAM_LINK}
             >
               <IG />
               its_yiching
+            </Contact.IconLink>
+            <Contact.IconLink
+              target="_blank"
+              rel="noreferrer"
+              href={FACEBOOK_LINK}
+            >
+              <FB />
+              Yiching Lin Illustration
             </Contact.IconLink>
             <Contact.IconLink
               target="_blank"
