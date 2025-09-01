d6583f4b644a7f9325446161bc2d738e571709a3
AmyHuang82 <a3315520@gmail.com>
2025-09-01 20:47:57 +0800

    fix: base url

--- utils/axiosUtils.js
+++ utils/axiosUtils.js
@@ -1,8 +1,6 @@
 /* eslint-disable no-restricted-imports */
 import axios from 'axios'
 
-const IS_DEV = process.env.NODE_ENV === 'development'
-
 export default axios.create({
-  baseURL: IS_DEV ? 'http://localhost:3000' : 'https://yichinglin.vercel.app',
+  baseURL: '',
 })
