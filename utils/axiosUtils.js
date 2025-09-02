01d17205f877bf125090771bba2e09f69ce2dc10
AmyHuang82 <a3315520@gmail.com>
2025-09-02 20:45:45 +0800

    fix: base url

--- utils/axiosUtils.js
+++ utils/axiosUtils.js
@@ -1,6 +1,25 @@
 /* eslint-disable no-restricted-imports */
 import axios from 'axios'
 
+function getBaseURL() {
+  if (process.env.NODE_ENV === 'development') {
+    return 'http://localhost:3000'
+  }
+
+  if (
+    process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview' &&
+    process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL
+  ) {
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`
+  }
+
+  if (process.env.NEXT_PUBLIC_VERCEL_URL) {
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  }
+
+  return 'https://yichinglin.vercel.app'
+}
+
 export default axios.create({
-  baseURL: '',
+  baseURL: getBaseURL(),
 })
